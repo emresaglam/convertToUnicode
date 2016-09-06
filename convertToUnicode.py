5a01299b544b722a397fca65259406679b823b29
Emre Saglam <emresaglam@gmail.com>
2016-09-06 15:40:23 -0700

    Initial commit

---
+++ convertToUnicode.py
@@ -0,0 +1,17 @@
+from io import open
+import argparse
+
+parser = argparse.ArgumentParser(description='Convert from ISO-8859-9 to UTF-8')
+parser.add_argument('--input', '-i', required=True)
+parser.add_argument('--output', '-o', required=True)
+args = parser.parse_args()
+
+reader = args.input
+writer = args.output
+
+writeFile = open(writer, 'w')
+
+with open(reader, encoding='ISO-8859-9') as fp:
+	for line in fp:
+		writeFile.write(line)
+
